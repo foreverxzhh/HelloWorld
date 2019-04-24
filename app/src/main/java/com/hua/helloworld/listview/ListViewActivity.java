96a4e28c9cb1aaa5071438e9a089993d33d0e39c
foreverxzhh <foreverxzhh@gmail.com>
2019-04-24 16:35:17 +0800

    list view demo

---
+++ app/src/main/java/com/hua/helloworld/listview/ListViewActivity.java
@@ -0,0 +1,36 @@
+package com.hua.helloworld.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.hua.helloworld.R;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+        lv = findViewById(R.id.lv);
+        lv.setAdapter(new MyListViewAdapter(this));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this, "点击pos：" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this, "长按pos：" + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+}
